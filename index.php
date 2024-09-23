3bec103975b10811357b4d1782678c8e9f879ddc
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-23 11:15:01 -0600

    Agregar thumnail y sección aliados

--- index.php
+++ index.php
@@ -3,17 +3,8 @@
 <section id="jumbotron" class="bg-jumbotron-home">
     <div class="container-fluid">
         <div class="row">
-            <div class="mb-5 col-lg-6 my-lg-auto order-lg-2 text-end">
-                <img
-                    src="<?php echo esc_url(
-                        get_template_directory_uri()
-                    ); ?>/assets/images/jumbotron/thumb-nosotros.png"
-                    alt=""
-                    class="img-fluid"
-                />
-            </div>
             <div
-                class="col-lg-5 offset-lg-1 my-lg-auto order-lg-1 text-center text-lg-start"
+                class="col-lg-5 offset-lg-1 my-lg-auto text-center text-lg-start"
             >
                 <div class="row mb-5">
                     <div class="col-12">
@@ -31,6 +22,25 @@
                         </a>
                     </div>
                 </div>
+            </div>
+            <div class="col-lg-6 my-lg-auto text-end">
+                <img
+                    src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/jumbotron/thumb-jumbotron-home.png"
+                    alt=""
+                    class="img-fluid"
+                />
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="aliados" class="pt-30">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1>Nuestros Aliados Sociales</h1>
             </div>
         </div>
     </div>
