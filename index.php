88c01230059ea33f4a5ab9f281756102ecb21397
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-23 11:02:41 -0600

    Armar estructura básica jumbotron home

--- index.php
+++ index.php
@@ -1,19 +1,39 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
-
-			<h1><?php esc_html_e( 'Latest Posts', 'html5blank' ); ?></h1>
-
-			<?php get_template_part( 'loop' ); ?>
-
-			<?php get_template_part( 'pagination' ); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<section id="jumbotron" class="bg-jumbotron-home">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="mb-5 col-lg-6 my-lg-auto order-lg-2 text-end">
+                <img
+                    src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/jumbotron/thumb-nosotros.png"
+                    alt=""
+                    class="img-fluid"
+                />
+            </div>
+            <div
+                class="col-lg-5 offset-lg-1 my-lg-auto order-lg-1 text-center text-lg-start"
+            >
+                <div class="row mb-5">
+                    <div class="col-12">
+                        <h2>Fundación</h2>
+                        <h1 class="fw-bold">Grupo Bafar</h1>
+                        <p>Una institución</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <a class="anchor arrow" href=".after-jumbotron">
+                            <i
+                                class="fa-solid fa-arrow-down-long moveUpDown"
+                            ></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
