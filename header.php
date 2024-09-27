95f520641cff49f0774d08bce61f1f0d3595f852
Luis Carlos Pando <hey@luiscarlospando.com>
2024-09-27 16:59:37 -0600

    Ir armando sección de figuras

--- header.php
+++ header.php
@@ -52,7 +52,7 @@
 
         <link rel="stylesheet" href="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/css/styles.css?v=2" />
+        ); ?>/assets/css/styles.css?v=3" />
     </head>
     <body <?php body_class(); ?>>
         <div class="redes-sociales">
