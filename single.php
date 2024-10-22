f54c9758beb654bb421e3db306dbd1c0a9fad63c
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-22 15:52:14 -0600

    Armar estructura single

--- single.php
+++ single.php
@@ -1,25 +1,57 @@
 <?php get_header(); ?>
 
-<section id="interna" class="py-60">
-    <div class="row">
-        <div class="col-12">
-            <?php if (have_posts()):
-                while (have_posts()):
-                    the_post(); ?>
+<section id="jumbotron" class="bg-jumbotron-1">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="mb-5 col-lg-6 my-lg-auto order-lg-2 text-end">
                 <?php the_post_thumbnail("thumb-noticia-grande", [
                     "class" => "img-fluid",
                 ]); ?>
-                <h1>
-                    <?php the_title(); ?>
-                </h1>
-                <?php the_content(); ?>
-            <?php
-                endwhile; ?>
-            <?php
-            else:
-                 ?>
-            <?php
-            endif; ?>
+            </div>
+            <div
+                class="col-lg-5 offset-lg-1 my-lg-auto order-lg-1 text-center text-lg-start"
+            >
+                <div class="row mb-5">
+                    <div class="col-12">
+                        <h1 class="fw-bold">Noticias</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <a class="anchor arrow" href=".after-jumbotron">
+                            <i
+                                class="fa-solid fa-arrow-down-long moveUpDown"
+                            ></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="interna" class="after-jumbotron py-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php if (have_posts()):
+                    while (have_posts()):
+                        the_post(); ?>
+                    <?php the_post_thumbnail("thumb-noticia-grande", [
+                        "class" => "img-fluid",
+                    ]); ?>
+                    <h1>
+                        <?php the_title(); ?>
+                    </h1>
+                    <?php the_content(); ?>
+                <?php
+                    endwhile; ?>
+                <?php
+                else:
+                     ?>
+                <?php
+                endif; ?>
+            </div>
         </div>
     </div>
 </section>
