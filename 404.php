7112b827e8f7d5c1a53f66b16e3e76d445dadec4
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-22 15:59:27 -0600

    Agregar estructura 404

--- 404.php
+++ 404.php
@@ -1,24 +1,28 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
-
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php esc_html_e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<section id="jumbotron" class="bg-jumbotron-1">
+    <div class="container-fluid">
+        <div class="row">
+            <div
+                class="col-lg-5 offset-lg-1 my-lg-auto order-lg-1 text-center text-lg-start"
+            >
+                <div class="row mb-5">
+                    <div class="col-12">
+                        <h1 class="fw-bold">Error 404: Página no encontrada<?php the_title(); ?></h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <a class="anchor arrow" href=".after-jumbotron">
+                            <i
+                                class="fa-solid fa-arrow-down-long moveUpDown"
+                            ></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
