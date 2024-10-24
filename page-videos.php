1197960972a9896f1ac5c90fd8a6e97987112677
Luis Carlos Pando <hey@luiscarlospando.com>
2024-10-24 09:40:20 -0600

    Agregar custom field para los videos

--- page-videos.php
+++ page-videos.php
@@ -38,175 +38,91 @@ get_header(); ?>
     </div>
 </section>
 
-<section
-    id="videos"
-    class="parallax-window py-60"
-    data-parallax="scroll"
-    data-image-src="<?php echo esc_url(
-        get_template_directory_uri()
-    ); ?>/assets/images/bg-videos.png"
->
-    <div class="container-fluid">
-        <div class="row mb-5">
-            <div
-                class="col-10 offset-1 col-lg-12 offset-lg-0 text-center"
-            >
-                <h1 id="titulo-ultima-noticia">
-                    <span class="fw-light">Torneo</span>
-                    <span class="fw-bold color-secondary comentario"
-                        >Úne-t</span
-                    >
-                </h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="swiper swiper-videos">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="overlay">
-                            <a
-                                href="javascript:void(0)"
-                                data-bs-toggle="modal"
-                                data-bs-target="#modal-video"
-                                data-bs-video-src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                            ></a>
-                        </div>
-                        <div class="card text-bg-light mb-3 rounded-4">
-                            <div class="card-body">
-                                <div class="ratio ratio-16x9">
-                                    <iframe
-                                        src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                                        frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowfullscreen
-                                    ></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="overlay">
-                            <a
-                                href="javascript:void(0)"
-                                data-bs-toggle="modal"
-                                data-bs-target="#modal-video"
-                                data-bs-video-src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                            ></a>
-                        </div>
-                        <div class="card text-bg-light mb-3 rounded-4">
-                            <div class="card-body">
-                                <div class="ratio ratio-16x9">
-                                    <iframe
-                                        src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                                        frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowfullscreen
-                                    ></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="overlay">
-                            <a
-                                href="javascript:void(0)"
-                                data-bs-toggle="modal"
-                                data-bs-target="#modal-video"
-                                data-bs-video-src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                            ></a>
-                        </div>
-                        <div class="card text-bg-light mb-3 rounded-4">
-                            <div class="card-body">
-                                <div class="ratio ratio-16x9">
-                                    <iframe
-                                        src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                                        frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowfullscreen
-                                    ></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="overlay">
-                            <a
-                                href="javascript:void(0)"
-                                data-bs-toggle="modal"
-                                data-bs-target="#modal-video"
-                                data-bs-video-src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                            ></a>
-                        </div>
-                        <div class="card text-bg-light mb-3 rounded-4">
-                            <div class="card-body">
-                                <div class="ratio ratio-16x9">
-                                    <iframe
-                                        src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                                        frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowfullscreen
-                                    ></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="overlay">
-                            <a
-                                href="javascript:void(0)"
-                                data-bs-toggle="modal"
-                                data-bs-target="#modal-video"
-                                data-bs-video-src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                            ></a>
-                        </div>
-                        <div class="card text-bg-light mb-3 rounded-4">
-                            <div class="card-body">
-                                <div class="ratio ratio-16x9">
-                                    <iframe
-                                        src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                                        frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowfullscreen
-                                    ></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="overlay">
-                            <a
-                                href="javascript:void(0)"
-                                data-bs-toggle="modal"
-                                data-bs-target="#modal-video"
-                                data-bs-video-src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                            ></a>
-                        </div>
-                        <div class="card text-bg-light mb-3 rounded-4">
-                            <div class="card-body">
-                                <div class="ratio ratio-16x9">
-                                    <iframe
-                                        src="https://www.youtube.com/embed/fHO3WQZs8Tk"
-                                        frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowfullscreen
-                                    ></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-button-next">
-                    <i class="fa-solid fa-arrow-right"></i>
-                </div>
-                <div class="swiper-button-prev">
-                    <i class="fa-solid fa-arrow-left"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<?php
+$args = [
+    "post_type" => "video",
+    "posts_per_page" => -1, // Retrieve all posts
+];
 
-<section id="slider-mas-contenido">
+$videos_query = new WP_Query($args);
+
+if ($videos_query->have_posts()): ?>
+    <section
+        id="videos"
+        class="parallax-window py-60"
+        data-parallax="scroll"
+        data-image-src="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/assets/images/bg-videos.png"
+    >
+        <?php
+        while ($videos_query->have_posts()):
+            $galerias_query->the_post(); ?>
+            <div class="container-fluid">
+                <div class="row mb-5">
+                    <div
+                        class="col-10 offset-1 col-lg-12 offset-lg-0 text-center"
+                    >
+                        <h1 id="titulo-ultima-noticia">
+                            <?php the_title(); ?>
+                        </h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="swiper swiper-videos">
+                        <div class="swiper-wrapper">
+                            <?php while (have_rows("videos")):
+
+                                the_row();
+                                $videoID = get_sub_field("video");
+                                ?>
+                                <div class="swiper-slide">
+                                    <div class="overlay">
+                                        <a
+                                            href="javascript:void(0)"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#modal-video"
+                                            data-bs-video-src="https://www.youtube.com/embed/<?php echo $videoID; ?>"
+                                        ></a>
+                                    </div>
+                                    <div class="card text-bg-light mb-3 rounded-4">
+                                        <div class="card-body">
+                                            <div class="ratio ratio-16x9">
+                                                <iframe
+                                                    src="https://www.youtube.com/embed/<?php echo $videoID; ?>"
+                                                    frameborder="0"
+                                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                    allowfullscreen
+                                                ></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
+                            endwhile; ?>
+                        </div>
+                        <div class="swiper-button-next">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                        <div class="swiper-button-prev">
+                            <i class="fa-solid fa-arrow-left"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php
+        endwhile;
+        wp_reset_postdata();
+
+    // Reset the post data
+    ?>
+    </section>
+<?php else: ?>
+    <p>No se encontraron galerías.</p>
+<?php endif;
+?>
+
+<!-- section id="slider-mas-contenido">
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -373,6 +289,6 @@ get_header(); ?>
             </div>
         </div>
     </div>
-</section>
+</section -->
 
 <?php get_footer(); ?>
