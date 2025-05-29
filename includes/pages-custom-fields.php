248cb99f9bc3210a1938c54f7cf9f8f0a8c53b33
Luis Carlos Pando <hey@luiscarlospando.com>
2025-05-29 12:59:53 -0600

    Implementar CF

--- includes/pages-custom-fields.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Custom fields
-$banner = get_field("banner_principal") ?: [];
-
-$texto_superior = $banner["texto_superior"] ?? "";
-$texto_inferior = $banner["texto_inferior"] ?? "";
-$imagen_de_fondo = $banner["imagen_de_fondo"] ?? "";
-$imagen_del_lado_derecho = $banner["imagen_del_lado_derecho"] ?? "";
-?>
